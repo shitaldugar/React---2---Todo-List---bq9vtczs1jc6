2261ab519a9377412c2ff22d5bcbf2d1eacc46fc
shitaldugar <shitalsonawat@gmail.com>
2021-11-13 23:16:57 +0530

    commit done

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,52 @@
 import React,{useState} from "react";
 import "./../styles/App.css";
 
-function App() 
-{
-	const[todo,setTodo]=useState("");
-	const[todoList,setTodoList]=useState([])
-	const[editable,setEditable]=useState(null);
-	const[editText,setEditText]=useState("");
+ function App() 
+ {
+	const [todo, setTodo] = useState('');
+    const [todoList, setTodoList] = useState([]);
+    const [editable, setEditable] = useState(null);
+    const [editText, setEditText] = useState('');
 
-	const handleChange=({target})=>{
-		setTodo(target.value);
-	}
-	const addToDo=(e)=>{
-		e.preventDefault();
+    const handleChange = ({target}) => {
+        setTodo(target.value);
+    }
 
-		if(todo!=""){
-			const myToDoList=todoList;
-			myToDoList.push(todo);
-			setTodoList(mytoDoList);
-		}else{
-			alert("Blank Task");
-		}
-		setTodo("");
-	}
-	const handleDelete=(myToDo)=>{
-		const updatedList=todoList.filter((todo)=>todo!=myToDo);
-		 setTodoList(updatedList);
-	}
-	const updateToDo=(mytoDo)=>{
-		const newList=todoList.map((todo)=>{
-			if(todo==myToDo){
-				if(editText!=""){
-					todo=editText;
-					return todo;
-				}
-			}
-			return todo;
-		});
-		setTodo("");
-		setEditText("");
-		setTodoList(newList);
-	}
-	return (
-	<div id="main">
-	<textarea id="task" onChange={handleChange} value={todo}/><br/>
+    const addToDo = (e) => {
+        e.preventDefault();
+
+        if(todo != '') {
+            const myToDoList = todoList;
+            myToDoList.push(todo);
+            setTodoList(myToDoList);
+        } else {
+            alert("Blank Task");
+        }
+        setTodo('');
+    }
+
+    const handleDelete = (myToDo) => {
+        const updatedList = todoList.filter((todo) => todo != myToDo);
+        setTodoList(updatedList);
+    }
+
+    const updateToDo = (myToDo) => {
+        const newList = todoList.map((todo) => {
+            if(todo == myToDo) {
+                if(editText != '') {
+                    todo = editText;
+                    return todo;
+                }
+            }
+            return todo;
+        });
+        setTodo('');
+        setEditText('');
+        setTodoList(newList);
+    }
+ 	return (
+ 	<div id="main">
+      <textarea id="task" onChange={handleChange} value={todo}/><br/>
             <button id="btn" onClick={addToDo}>Add Task </button>
 
             {todoList.map((myToDo, index) => {
@@ -70,7 +73,3 @@ function App()
  	);
  }
 export default App;
-
-
-
-
